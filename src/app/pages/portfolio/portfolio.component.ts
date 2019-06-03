589b60674e659a6a8a22120bc94028e29ca27171
Luis Hernandez <ziul.ch@gmail.com>
2019-06-03 17:18:25 -0400

    Added Animation Portfolio

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { slideInStagger } from '../../route-animations';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  styleUrls: ['./portfolio.component.scss'],
+  animations: [slideInStagger]
 })
 export class PortfolioComponent implements OnInit {
 
