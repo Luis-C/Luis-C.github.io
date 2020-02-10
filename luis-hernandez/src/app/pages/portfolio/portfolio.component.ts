9be7f29fed8270fc450298168217b934fc45f945
Luis Hernandez <ziul.ch@gmail.com>
2020-02-09 20:58:24 -0500

    Rebuilt Personal Project
    - Angular 9 Update
    - Removed animations

--- luis-hernandez/src/app/pages/portfolio/portfolio.component.ts
+++ luis-hernandez/src/app/pages/portfolio/portfolio.component.ts
@@ -1,17 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { slideInStagger } from '../../route-animations';
+import { Component, OnInit } from "@angular/core";
+import { slideInStagger } from "../../route-animations";
 
 @Component({
-  selector: 'app-portfolio',
-  templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss'],
+  selector: "app-portfolio",
+  templateUrl: "./portfolio.component.html",
+  styleUrls: ["./portfolio.component.scss"],
   animations: [slideInStagger]
 })
 export class PortfolioComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
