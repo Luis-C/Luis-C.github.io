cea772ee0af231da4c332a43cf1140d281b993fa
Luis Hernandez <ziul.ch@gmail.com>
2020-07-11 15:54:01 -0400

    Reusable schema for code projects

--- luis-hernandez/src/app/pages/portfolio/portfolio.component.ts
+++ luis-hernandez/src/app/pages/portfolio/portfolio.component.ts
@@ -5,9 +5,18 @@ import { slideInStagger } from "../../route-animations";
   selector: "app-portfolio",
   templateUrl: "./portfolio.component.html",
   styleUrls: ["./portfolio.component.scss"],
-  animations: [slideInStagger]
+  animations: [slideInStagger],
 })
 export class PortfolioComponent implements OnInit {
+  codeProjects = [
+    {
+      title: "testname",
+      details: "test info",
+      imgSrc: "assets/images/cu.png",
+      codeSrc: "https://github.com/Luis-C/Luis-C.github.io",
+    },
+  ];
+
   constructor() {}
 
   ngOnInit() {}
